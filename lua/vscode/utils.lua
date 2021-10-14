4a4561d2c0ec5088cf137e958bba949ee6b35392
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-10-15 01:07:28 +0800

    Fix vim.g.colors_name

--- lua/vscode/utils.lua
+++ lua/vscode/utils.lua
@@ -4,24 +4,22 @@ local utils = {}
 
 utils.highlight = function(group, highlights)
 	local fg = highlights[1] and " guifg=" .. highlights[1] or " guifg=NONE"
-	local bg = highlights[2] and " guibg=" .. highlights[2]  or " guibg=NONE"
+	local bg = highlights[2] and " guibg=" .. highlights[2] or " guibg=NONE"
 	local attr = highlights[3] and " gui=" .. highlights[3] or " gui=NONE"
 	local sp = highlights[4] and " guisp=" .. highlights[4] or ""
 	vim.api.nvim_command("highlight " .. group .. fg .. bg .. attr .. sp)
 end
 
-utils.load = function ()
-
-	vim.g.colors_name = "vscode"
-
+utils.load = function()
 	vim.cmd("hi clear")
-    if vim.fn.exists("syntax_on") then
+	if vim.fn.exists("syntax_on") then
 		vim.cmd("syntax reset")
 	end
-    
+
 	vim.o.background = "dark"
-    vim.o.termguicolors = true
-		
+	vim.o.termguicolors = true
+	vim.g.colors_name = "vscode"
+
 	local syntax = theme.load_syntax()
 
 	for group, color in pairs(syntax) do
