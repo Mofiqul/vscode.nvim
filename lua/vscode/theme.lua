79c330918ffab927d814059a7aa123103e49fb7b
Narawit Rakket <narawitrakket@hotmail.com>
2021-12-12 17:45:19 +0700

    feat: add support for barbar.nvim

--- lua/vscode/theme.lua
+++ lua/vscode/theme.lua
@@ -405,6 +405,25 @@ theme.load_syntax = function()
 		BufferLineIndicatorSelected = { c.vscLeftDark, nil, 'none', nil },
 		BufferLineFill = { nil, c.vscLeftDark, 'none', nil },
 
+		-- BarBar
+		BufferCurrent = { c.vscFront, c.vscTabCurrent, 'none', nil },
+		BufferCurrentIndex = { c.vscFront, c.vscTabCurrent, 'none', nil },
+		BufferCurrentMod = { c.vscYellowOrange, c.vscTabCurrent, 'none', nil },
+		BufferCurrentSign = { c.vscFront, c.vscTabCurrent, 'none', nil },
+		BufferCurrentTarget = { c.vscRed, c.vscTabCurrent, 'none', nil },
+		BufferVisible = { c.vscGray, c.vscTabCurrent, 'none', nil },
+		BufferVisibleIndex = { c.vscGray, c.vscTabCurrent, 'none', nil },
+		BufferVisibleMod = { c.vscYellowOrange, c.vscTabCurrent, 'none', nil },
+		BufferVisibleSign = { c.vscGray, c.vscTabCurrent, 'none', nil },
+		BufferVisibleTarget = { c.vscRed, c.vscTabCurrent, 'none', nil },
+		BufferInactive = { c.vscGray, c.vscTabOther, 'none', nil },
+		BufferInactiveIndex = { c.vscGray, c.vscTabOther, 'none', nil },
+		BufferInactiveMod = { c.vscYellowOrange, c.vscTabOther, 'none', nil },
+		BufferInactiveSign = { c.vscGray, c.vscTabOther, 'none', nil },
+		BufferInactiveTarget = { c.vscRed, c.vscTabOther, 'none', nil },
+		BufferTabpages = { c.vscFront, c.vscTabOther, 'none', nil },
+		BufferTabpagesFill = { c.vscFront, c.vscTabOther, 'none', nil },
+
 		-- IndentBlankLine
 		IndentBlanklineContextChar = { c.vscRed, nil, 'nocombine', nil },
 
