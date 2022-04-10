ee33921a9586352533d6799359e8bca6d0da055e
askfiy <c2323182108@gmail.com>
2022-04-10 22:11:20 +0800

    change lualine mode colors, add command mode colors

--- lua/lualine/themes/vscode.lua
+++ lua/lualine/themes/vscode.lua
@@ -3,50 +3,60 @@
 local vscode = {}
 local colors = {}
 
-if vim.g.vscode_style == 'dark' then
-    colors.bg = '#262626'
-    colors.fg = '#ffffff'
-    colors.red = '#f44747'
-    colors.green = '#619955'
-    colors.blue = '#0a7aca'
-    colors.lightblue = '#5CB6F8'
-    colors.yellow = '#ffaf00'
+if vim.g.vscode_style == "dark" then
+    colors.bg = "#262626"
+    colors.bg2 = "#373737"
+    colors.fg = "#ffffff"
+    colors.red = "#f44747"
+    colors.green = "#4EC9B0"
+    colors.blue = "#0a7aca"
+    colors.lightblue = "#5CB6F8"
+    colors.yellow = "#ffaf00"
+    colors.pink = "#DDB6F2"
 else
-    colors.bg = '#E5EBF1'
-    colors.fg = '#343434'
-    colors.red = '#FF0000'
-    colors.green = '#008000'
-    colors.blue = '#AF00DB'
-    colors.lightblue = '#0451A5'
-    colors.yellow = '#ffaf00'
+    colors.bg = "#F5F5F5"
+    colors.bg2 = "#E4E4E4"
+    colors.fg = "#343434"
+    colors.red = "#FF0000"
+    colors.green = "#008000"
+    colors.blue = "#AF00DB"
+    colors.lightblue = "#0451A5"
+    colors.yellow = "#ffaf00"
+    colors.pink = "#FFA3A3"
 end
 
 vscode.normal = {
-    b = { fg = colors.blue, bg = colors.bg },
-    a = { fg = vim.g.vscode_style == 'dark' and colors.fg or colors.bg, bg = colors.blue, gui = 'bold' },
-    c = { fg = colors.fg, bg = colors.bg },
+    b = {fg = colors.blue, bg = colors.bg2},
+    a = {fg = vim.g.vscode_style == "dark" and colors.fg or colors.bg, bg = colors.blue, gui = "bold"},
+    c = {fg = colors.fg, bg = colors.bg}
 }
 
 vscode.visual = {
-    a = { fg = colors.bg, bg = colors.lightblue, gui = 'bold' },
-    b = { fg = colors.lightblue, bg = colors.bg },
+    a = {fg = colors.bg, bg = colors.yellow, gui = "bold"},
+    b = {fg = colors.yellow, bg = colors.bg2}
 }
 
 vscode.inactive = {
-    b = { fg = colors.bg, bg = colors.fg },
-    a = { fg = colors.fg, bg = colors.bg, gui = 'bold' },
+    b = {fg = colors.bg, bg = colors.fg},
+    a = {fg = colors.fg, bg = colors.bg, gui = "bold"}
 }
 
 vscode.replace = {
-    b = { fg = colors.yellow, bg = colors.bg },
-    a = { fg = vim.g.vscode_style == 'dark' and colors.bg or colors.fg, bg = colors.yellow, gui = 'bold' },
-    c = { fg = colors.fg, bg = colors.bg },
+    b = {fg = colors.red, bg = colors.bg2},
+    a = {fg = vim.g.vscode_style == "dark" and colors.bg or colors.fg, bg = colors.red, gui = "bold"},
+    c = {fg = colors.fg, bg = colors.bg}
 }
 
 vscode.insert = {
-    a = { fg = vim.g.vscode_style == 'dark' and colors.bg or colors.fg, bg = colors.yellow, gui = 'bold' },
-    b = { fg = colors.yellow, bg = colors.bg },
-    c = { fg = colors.fg, bg = colors.bg },
+    a = {fg = vim.g.vscode_style == "dark" and colors.bg or colors.fg, bg = colors.green, gui = "bold"},
+    b = {fg = colors.green, bg = colors.bg2},
+    c = {fg = colors.fg, bg = colors.bg}
+}
+
+vscode.command = {
+    b = {fg = colors.pink, bg = colors.bg2},
+    a = {fg = vim.g.vscode_style == "dark" and colors.bg or colors.fg, bg = colors.pink, gui = "bold"},
+    c = {fg = colors.fg, bg = colors.bg}
 }
 
 return vscode
