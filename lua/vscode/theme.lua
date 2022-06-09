c27f27004c72b8165bb0971275d99ebf78925caf
hexh <250786313@qq.com>
2022-06-09 20:58:17 +0800

    perf: update

--- lua/vscode/theme.lua
+++ lua/vscode/theme.lua
@@ -1,4 +1,5 @@
 local colors = require('vscode.colors')
+local lualine = require('lualine.themes.vscode')
 local theme = {}
 
 theme.load_syntax = function()
@@ -428,8 +429,22 @@ theme.load_syntax = function()
         NvimTreeOpenedFolderName = { nil, no_nvimtree_bg and c.vscCursorDarkDark or c.vscLeftDark, 'none', nil },
 
         -- Bufferline
-        BufferLineIndicatorSelected = { c.vscLeftDark, nil, 'none', nil },
-        BufferLineFill = { nil, c.vscLeftDark, 'none', nil },
+        BufferLineFill = { nil, lualine.normal.b.bg, 'none', nil },
+        BufferLineDuplicate = { nil, lualine.normal.b.bg, 'italic', nil },
+        BufferLineDuplicateSelected = { lualine.insert.a.bg, lualine.normal.a.bg, 'italic', nil },
+        BufferLineTab = { lualine.normal.b.fg, lualine.normal.b.bg, 'none', nil },
+        BufferLineTabSelected = { nil, lualine.normal.a.bg, 'none', nil },
+        BufferLineTabClose = { lualine.normal.b.fg, lualine.normal.b.bg, 'none', nil },
+        BufferLineCloseButton = { lualine.normal.b.fg, lualine.normal.b.bg, 'none', nil },
+        BufferLineCloseButtonSelected = { lualine.normal.a.fg, lualine.normal.a.bg, 'none', nil },
+        BufferLineBufferSelected = { lualine.normal.a.fg, lualine.normal.a.bg, 'bold', nil },
+        BufferLineBackground = { lualine.normal.a.fg, lualine.normal.b.bg, 'none', nil },
+        BufferLineBuffer = { lualine.normal.b.fg, lualine.normal.b.bg, 'none', nil },
+        BufferLineModified = { nil, lualine.normal.b.bg, 'none', nil },
+        BufferLineModifiedSelected = { nil, lualine.normal.a.bg, 'none', nil },
+        BufferLineSeparator = { lualine.normal.b.bg, lualine.normal.b.bg, 'none', nil },
+        BufferLineSeparatorSelected = { lualine.normal.b.bg, lualine.normal.a.bg, 'none', nil },
+        BufferLineIndicatorSelected = { lualine.normal.a.fg, lualine.normal.a.bg, 'none', nil },
 
         -- BarBar
         BufferCurrent = { c.vscFront, c.vscTabCurrent, 'none', nil },
