22976346e84ff61bc646b4c848e01fca24a072c8
daephx <daephxdev@gmail.com>
2023-03-22 15:03:45 -0700

    fix: global opts due to config not being initialized
    Fixes #122
    
    Also allows the `user_opts` parameter to be optional by substituting it
    with an empty table if nil before applying `vim.tbl_extend`.

--- lua/vscode/config.lua
+++ lua/vscode/config.lua
@@ -10,7 +10,7 @@ local defaults = {
 
 config.opts = {}
 
----@param user_opts table
+---@param user_opts? table
 config.setup = function(user_opts)
     -- backwards compatibility: let users still set settings with global vars
     local global_settings_opts = vim.tbl_extend('force', defaults, {
@@ -20,12 +20,15 @@ config.setup = function(user_opts)
     })
 
     -- but override global vars settings with setup() settings
-    config.opts = vim.tbl_extend('force', global_settings_opts, user_opts)
+    config.opts = vim.tbl_extend('force', global_settings_opts, user_opts or {})
 
     -- setting transparent to true removes the default background
     if config.opts.transparent then
         config.opts.color_overrides.vscBack = 'NONE'
     end
 end
+
+-- initialize config
+config.setup()
 
 return config
