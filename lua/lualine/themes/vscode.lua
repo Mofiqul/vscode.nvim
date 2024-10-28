90c68090e03df96f6edb80f84f7ba29cb20198d3
tuyenpm9 <>
2024-10-28 12:52:19 +0700

    update files

--- lua/lualine/themes/vscode.lua
+++ lua/lualine/themes/vscode.lua
@@ -1,7 +1,7 @@
 -- Copyright (c) 2020-2021 Mofiqul Islam
 -- MIT license, see LICENSE for more details.
 local config = require('vscode-theme.config')
-local vscode = {}
+local vscode-theme = {}
 local colors = {}
 
 if vim.o.background == 'dark' then
@@ -28,45 +28,45 @@ else
     colors.pink = '#FFA3A3'
 end
 
-vscode.normal = {
+vscode-theme.normal = {
     a = { fg = vim.o.background == 'dark' and colors.fg or colors.bg, bg = colors.blue, gui = 'bold' },
     b = { fg = colors.blue, bg = config.opts.transparent and 'NONE' or colors.bg2 },
     c = { fg = colors.fg, bg = config.opts.transparent and 'NONE' or colors.bg },
 }
 
-vscode.visual = {
+vscode-theme.visual = {
     a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' },
     b = { fg = colors.yellow, bg = config.opts.transparent and 'NONE' or colors.bg },
 }
 
-vscode.inactive = {
+vscode-theme.inactive = {
     a = { fg = colors.fg, bg = colors.bg, gui = 'bold' },
     b = { fg = colors.inactive, bg = config.opts.transparent and 'NONE' or colors.bg },
     c = { fg = colors.inactive, bg = config.opts.transparent and 'NONE' or colors.bg },
 }
 
-vscode.replace = {
+vscode-theme.replace = {
     a = { fg = vim.o.background == 'dark' and colors.bg or colors.fg, bg = colors.red, gui = 'bold' },
     b = { fg = colors.red, bg = config.opts.transparent and 'NONE' or colors.bg2 },
     c = { fg = colors.fg, bg = config.opts.transparent and 'NONE' or colors.bg },
 }
 
-vscode.insert = {
+vscode-theme.insert = {
     a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
     b = { fg = colors.green, bg = config.opts.transparent and 'NONE' or colors.bg2 },
     c = { fg = colors.fg, bg = config.opts.transparent and 'NONE' or colors.bg },
 }
 
-vscode.terminal = {
+vscode-theme.terminal = {
     a = { fg = vim.o.background == 'dark' and colors.bg or colors.fg, bg = colors.green, gui = 'bold' },
     b = { fg = colors.fg, bg = config.opts.transparent and 'NONE' or colors.bg2 },
     c = { fg = colors.fg, bg = config.opts.transparent and 'NONE' or colors.bg },
 }
 
-vscode.command = {
+vscode-theme.command = {
     a = { fg = vim.o.background == 'dark' and colors.bg or colors.fg, bg = colors.pink, gui = 'bold' },
     b = { fg = colors.pink, bg = config.opts.transparent and 'NONE' or colors.bg2 },
     c = { fg = colors.fg, bg = config.opts.transparent and 'NONE' or colors.bg },
 }
 
-return vscode
+return vscode-theme
